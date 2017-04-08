a3951107f049375191ecb1556f78892c3ce55f4d
Tom Owen <tom@Toms-MBP.hsd1.nj.comcast.net>
2017-04-07 21:18:06 -0400

    Added links to our hw2 pages

--- index.php
+++ index.php
@@ -4,25 +4,30 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="Author" content="Tom Owen Rithvik Mandalapu Steve Pham Michael Robinson">
     <title>tWOWter</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap-theme.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bitter:400,700">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
-    <link rel="stylesheet" href="assets/css/styles.min.css">
+    <link rel="stylesheet" href="assets/css/Footer-Dark.css">
+    <link rel="stylesheet" href="assets/css/Header-Dark.css">
+    <link rel="stylesheet" href="assets/css/Login-Form-Dark.css">
+    <link rel="stylesheet" href="assets/css/Registration-Form-with-Photo.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
 </head>
 
 <body>
     <div>
-        <div class="header-dark" style="background-image:url(&quot;assets/img/warlock.jpg&quot;);background-size:auto;">
+        <div class="header-dark" style="background-image:url(&quot;assets/img/warlock-02_162841-800x600.jpg&quot;);background-size:auto;">
             <nav class="navbar navbar-default navigation-clean-search">
                 <div class="container">
-                    <div class="navbar-header"><a class="navbar-brand navbar-link" href="index.php">tWOWter </a>
+                    <div class="navbar-header"><a class="navbar-brand navbar-link" href="index.html">tWOWter </a>
                         <button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
                     </div>
                     <div class="collapse navbar-collapse" id="navcol-1">
                         <ul class="nav navbar-nav"></ul>
-                        <p class="navbar-text navbar-right"><a class="navbar-link login" href="login.php">Log In</a> <a class="btn btn-default action-button" role="button" href="signup.php">Sign Up</a></p>
+                        <p class="navbar-text navbar-right"><a class="navbar-link login" href="login.html">Log In</a> <a class="btn btn-default action-button" role="button" href="#">Sign Up</a></p>
                     </div>
                 </div>
             </nav>
@@ -45,7 +50,10 @@
                     <div class="col-md-3 col-md-pull-6 col-sm-4 item">
                         <h3>About</h3>
                         <ul>
-                            <li><a href="#">Team</a></li>
+                            <li><a href="http://elvis.rowan.edu/~owent0/web/hw2-list/hw2-list.html"><h4>Team Members:</h4>Tom Owen</a></li>
+                            <li><a href="http://elvis.rowan.edu/~mandalapr0/web/hw2-list/hw2-list.html">Rithvik Mandalpu</a></li>
+                            <li><a href="http://elvis.rowan.edu/~phams0/web/hw2-list/hw2-list.html">Steve Pham</a></li>
+                            <li><a href="http://elvis.rowan.edu/~robinsonm5/web/hw2-list/hw2-list.html">Michael Robinson</a></li>
                         </ul>
                     </div>
                     <div class="col-md-12 col-sm-4 item social"><a href="#"><i class="icon ion-social-facebook"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-instagram"></i></a></div>
