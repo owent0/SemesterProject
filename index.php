63b9741bd2de9bdcac3c5a2cf1d5c635444b6819
Tom Owen <owent9524@gmail.com>
2017-04-27 08:34:33 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -1,72 +1,112 @@
 <!DOCTYPE html>
-<html style="background-color:#282d32;">
+<html>
+	<head>
+		<title>tWOWter</title>
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="Author" content="Tom Owen, Rithvik Mandalapu, Steve Pham, Michael Robinson">
-    <title>tWOWter</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap-theme.min.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bitter:400,700">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
-    <link rel="stylesheet" href="assets/css/Footer-Dark.css">
-    <link rel="stylesheet" href="assets/css/Header-Dark.css">
-    <link rel="stylesheet" href="assets/css/Login-Form-Clean.css">
-    <link rel="stylesheet" href="assets/css/Login-Form-Dark.css">
-    <link rel="stylesheet" href="assets/css/Registration-Form-with-Photo.css">
-    <link rel="stylesheet" href="assets/css/styles.css">
-</head>
+		<meta name="Author" content="Tom Owen, Steve Pham, Rithvik Mandalapu, and Michael Robinson">
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
-<body style="background-color:rgb(40,45,50);">
-    <div>
-        <div class="header-dark" style="background-size:auto;background-color:rgb(40,45,50);">
-            <nav class="navbar navbar-default navigation-clean-search">
-                <div class="container">
-                    <div class="navbar-header"><a class="navbar-brand navbar-link" href="index.php">tWOWter <i class="fa fa-magic"></i></a>
-                        <button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
-                    </div>
-                    <div class="collapse navbar-collapse" id="navcol-1">
-                        <ul class="nav navbar-nav"></ul>
-                        <p class="navbar-text navbar-right"><a class="navbar-link login" href="login.php">Log In</a> <a class="btn btn-default action-button" role="button" href="signup.php">Sign Up</a></p>
-                    </div>
-                </div>
-            </nav>
-            <div class="container hero">
-                <div class="row">
-                    <div class="col-md-8 col-md-offset-2">
-                        <h1 class="text-center">The #1 place for WOW lovers.</h1></div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="footer-dark">
-        <footer>
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-6 col-md-push-6 item text">
-                        <h3>tWOWter </h3>
-                        <p>A social media site where all WOW lovers can get together and talk about WOW.</p>
-                    </div>
-                    <div class="col-md-3 col-md-pull-6 col-sm-4 item">
-                        <h3>About</h3>
-                        <ul>
-                            <li><a href="http://elvis.rowan.edu/~owent0/web/hw2-list/hw2-list.html"><h4>Team Members:</h4>Tom Owen</a>
-                                <a href="https://github.com/owent0" style="padding-left:10px;"> <i class="fa fa-github"></i></a>
-                            </li>
-                            <li><a href="http://elvis.rowan.edu/~mandalapr0/web/hw2-list/hw2-list.html">Rithvik Mandalpu</a></li>
-                            <li><a href="http://elvis.rowan.edu/~phams0/web/hw2-list/hw2-list.html">Steve Pham</a></li>
-                            <li><a href="http://elvis.rowan.edu/~robinsonm5/web/hw2-list/hw2-list.html">Michael Robinson</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <p class="copyright">tWOWter 2017</p>
-            </div>
-        </footer>
-    </div>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</body>
+		<link rel="stylesheet" type="text/css" href="styles.css">
 
+		<!-- jQuery library -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+
+		<!-- Latest compiled JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	</head>
+	<body>
+		<?php
+
+		?>
+
+		<div class="main">
+			<nav class="navbar navbar-inverse">
+			  <div class="container-fluid">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span> 
+						</button>
+						<a class="navbar-brand" href="#">tWOWter</a>
+					</div>
+					<div class="collapse navbar-collapse" id="myNavbar">
+						<ul class="nav navbar-nav">
+							<li class="active"><a href="#">Home</a></li>
+							<li><a href="#">About</a></li>
+						</ul>
+						<ul class="nav navbar-nav navbar-right">
+							<li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+							<li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+
+			<div class="container">
+				<h1>Login or Create Your Account</h1>
+				<!-- Start Login form -->
+				<form>
+					<div class="row">
+						<div class="col-md-6">
+								<div class="form-group">
+									<h2>New Account:</h2>
+								</div>
+								<div class="form-group">
+									<label for="usrnm">Username:</label>
+									<input type="text" name="username" class="form-control" id="createUsername" placeholder="Your awesome username" required/>
+								</div>
+								<div class="form-group">
+									<label for="email">Email:</label>
+									<input type="email" placeholder="Email" class="form-control" id="createEmail" required/>
+								</div>
+
+								<div class="form-group">
+									<label for="pwd">Password:</label>
+									<input type="password" placeholder="password" class="form-control" id="createPassword" required/>
+								</div>
+
+								<button type="submit" class="btn btn-default">Create Account</button>
+								<!-- We may need this 
+								<input type="submit" onclick="myFunction()" value="Create Account">
+								-->
+						</div>
+						<div class="col-md-6">
+							<div class="form-group">
+								<h2>Login:</h2>
+							</div>
+
+							<div class="form-group">
+								<label for="usrnm">Username:</label>
+								<input type="text" name="username" class="form-control" id="createUsername" placeholder="Your awesome username" required/>
+							</div>
+
+							<div class="form-group">
+								<label for="pwd">Password:</label>
+								<input type="password" placeholder="password" class="form-control" id="createPassword" required/>
+							</div>	
+
+							<button type="submit" class="btn btn-default">Login</button>
+							<!-- We may need this 
+								<input type="submit" onclick="myFunction()" value="Login">
+							-->
+						</div>
+					</div>
+				</form>		
+				<!-- End Login form -->		
+			</div>
+			
+					
+
+
+		</div>
+
+		<a href="http://validator.w3.org/check/referer">
+            <img style="border:0;width:70px;height:20px;float:left;padding-right:10px;" src="http://www.w3.org/Icons/valid-xhtml11" alt="Valid XHTML 1.1!">
+        </a>
+        <a href="http://jigsaw.w3.org/css-validator/check/referer?profile=css3">
+            <img style="border:0;width:70px;height:20px;float:left;padding-right:10px;" src="http://jigsaw.w3.org/css-validator/images/vcss" alt="Valid CSS!">
+        </a>
+	</body>
 </html>
